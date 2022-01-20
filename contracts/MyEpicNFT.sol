2cffa29b4b08da707dbba87b7fa3083d79f8dd26
Hamza Zeghari <zeghari.hamzaa@gmail.com>
2022-01-20 15:10:40 +0100

    Setup local environment up and runing

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSE
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract MyEpicNFT {
+    constructor(){
+        console.log("This is my NFT contract. Whoa!");
+    }
+}
